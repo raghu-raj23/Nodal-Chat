94941d1611125147b17d14e5afa7ecaaceffc2db
raghu-raj23 <raghuraj.muni@gmail.com>
2026-04-14 22:35:43 +0530

    code fix

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,13 @@ import { connectDB } from "./src/config/database";
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`);
+connectDB()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Failed to connect to the database:", error);
+		process.exit(1);
 	});
-});
